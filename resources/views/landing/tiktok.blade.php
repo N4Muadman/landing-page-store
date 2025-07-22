d006d119810eb9e9590b286d0cb955da6cfc99f3
N4Muadman <126045945+N4Muadman@users.noreply.github.com>
2025-07-22 23:28:46 +0700

    update

--- resources/views/landing/tiktok.blade.php
+++ resources/views/landing/tiktok.blade.php
@@ -29,33 +29,7 @@
     <link rel="stylesheet" href="/asset-tiktok/css/style.css">
     <link rel="preload" href="/asset-tiktok/js/main.js" as="script">
 
-    <!-- Facebook Pixel Code -->
-    <script>
-        ! function(f, b, e, v, n, t, s) {
-            if (f.fbq) return;
-            n = f.fbq = function() {
-                n.callMethod ?
-                    n.callMethod.apply(n, arguments) : n.queue.push(arguments)
-            };
-            if (!f._fbq) f._fbq = n;
-            n.push = n;
-            n.loaded = !0;
-            n.version = '2.0';
-            n.queue = [];
-            t = b.createElement(e);
-            t.async = !0;
-            t.src = v;
-            s = b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t, s)
-        }(window, document, 'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-        fbq('init', '{{$product->pixel_fb}}');
-        fbq('track', 'PageView');
-    </script>
-    <noscript>
-        <img height="1" width="1" style="display:none"
-            src="https://www.facebook.com/tr?id={{$product->pixel_fb}}&ev=PageView&noscript=1" />
-    </noscript>
+    {!! $product->pixel_fb !!}
 </head>
 
 <body class="lazyload">
